c17b8108e6c4379adc3c04fd0d5b648d62100364
mirhasani <mirhasani.kh@gmail.com>
2024-02-25 19:35:31 +0330

    update profile

--- AdminDash/src/pages/ProfilePage.vue
+++ AdminDash/src/pages/ProfilePage.vue
@@ -1,13 +1,7 @@
 <template>
-  <q-page padding >
-    <div class="row justify-center">
-      <div class="col-auto text-center">
-        <q-avatar size="80px">
-          <q-img  src="/img/man.webp" />
-        </q-avatar>
-        <h1 class="text-h4 q-ma-none q-mt-lg">پروفایل من</h1>
-      </div>
-    </div>
+  <q-page class="flex flex-center">
+
+<h4>profile</h4>
   </q-page>
 </template>
 
